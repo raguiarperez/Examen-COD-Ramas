74fdc10e0ac4fefa0c35cff744009a613c4ee574
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2018-11-15 09:17:05 +0100

    E

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -8,6 +8,15 @@ public class ExCOD1Av {
 
     public static void main(String[] args) {
            
+
+        int x = 0;
+        int y = 0;
+        int Sum;
+        
+        Sum=x+y;
+        
+        System.out.println("la suma es de"+Sum);
+
     }
 
 }
