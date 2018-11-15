892aee8f19d4e2a67a196625df6c78df2f964503
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2018-11-15 09:20:23 +0100

    F

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -7,15 +7,21 @@ package excod1av;
 public class ExCOD1Av {
 
     public static void main(String[] args) {
-           
 
         int x = 0;
         int y = 0;
         int Sum;
-        
-        Sum=x+y;
-        
-        System.out.println("la suma es de"+Sum);
+
+        int Resta;
+
+        Sum = x + y;
+        Resta = x - y;
+
+        System.out.println("la suma es de " + Sum + "\n la resta es de " + Resta);
+
+        Sum = x + y;
+
+        System.out.println("la suma es de" + Sum);
 
     }
 
