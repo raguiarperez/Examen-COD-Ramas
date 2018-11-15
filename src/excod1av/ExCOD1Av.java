f5e156504b71bd386448e3465eda8c05a0bc500e
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2018-11-15 09:15:52 +0100

    C

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -2,12 +2,19 @@ package excod1av;
 
 /**
  *
- * @author damian
+ * @author RAFA
  */
 public class ExCOD1Av {
 
     public static void main(String[] args) {
            
+        int x = 0;
+        int y = 0;
+        int Sum;
+        
+        Sum=x+y;
+        
+        System.out.println(Sum);
     }
 
 }
