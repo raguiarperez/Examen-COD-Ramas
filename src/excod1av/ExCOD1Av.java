728df21b14b9582ed9972e5af406134f176fbf91
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2018-11-15 09:13:29 +0100

    B

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package excod1av;
 
 /**
@@ -11,11 +6,8 @@ package excod1av;
  */
 public class ExCOD1Av {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+           
     }
-    
+
 }
